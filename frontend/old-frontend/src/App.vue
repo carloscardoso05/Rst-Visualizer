bedb3e803cdd3c3c81d210ab174c7ba08e234644
Carlos Cardoso <cvcardos5@gmail.com>
2025-05-24 15:06:20 -0300

    update

--- frontend/old-frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
-<template>
-  <div data-theme="light" class="min-h-screen">
-    <RouterView />
-  </div>
-</template>
